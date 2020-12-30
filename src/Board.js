84a546572346bf63e58a40fe32ed6f8043e021b3
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-12-30 15:23:58 -0800

    board

--- src/Board.js
+++ src/Board.js
@@ -30,11 +30,19 @@ import './Board.css';
  **/
 
 class Board extends Component {
+  static defaultProps = {
+    title: 'Lights Out',
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  }
 
   constructor(props) {
     super(props);
-
-    // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,6 +50,13 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row)
+    }
     return board
   }
 
